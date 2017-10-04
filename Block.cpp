44289a1ddb17d2c17cdd763af58bf56019ed97fd
Timothy Ko <tko@spikes.com>
2017-10-04 15:26:04 -0500

    added Blocks

---
+++ Block.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Block {
+    public:
+        Block(int prevHas, string trans  );
+        int getPreviousHash(void);
+        int getHash(void);
+        hash<string> hash_fn;
+
+        void toString(void);
+    private:
+        int previousHash;
+        int blockHash;
+        string transactions;
+};
+Block::Block( int prevHash, string trans ) {
+    this -> previousHash = prevHash;
+    this -> transactions = trans;
+    int contents[] = {hash_fn(trans),previousHash};
+    cout << "ContentS" << contents << endl;
+    // this -> blockHash = hash_fn(contents.to_bytes);
+    // cout << blockHash;
+
+}
+int Block::getPreviousHash(void) {
+    return previousHash;
+}
+int Block::getHash(void) {
+    return blockHash;
+}
+void Block::toString(void) {
+    cout << "Block hash: " << this -> blockHash << "prevHash: " << this -> previousHash;
+}
+
+// string Block::getTransactions(void)  {
+//     return Block::transactions;
+// }
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "./Block.cpp"
+
+using namespace std;
+
+int main() {
+    string str1 = "Hello";
+    int i = 1;
+    
+    Block b1 = Block(0,str1);
+    return 0;
+} 
