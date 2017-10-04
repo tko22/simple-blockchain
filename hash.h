7004efe2f84fb77736fa5ee03fe935472dbd3926
Timothy Ko <tko@spikes.com>
2017-10-04 17:10:57 -0500

    added hash

---
+++ hash.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+#include <openssl/sha.h>
+#include <openssl/ripemd.h>
+using namespace std;
+
+
+string sha256(const string str)
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+    stringstream ss;
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        ss << hex << setw(2) << setfill('0') << (int)hash[i];
+    }
+    return ss.str();
+}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <string>
 #include "./Block.cpp"
+// #include "hash.h"
 
 using namespace std;
 
 int main() {
     string str1 = "Hello";
-    int i = 1;
+    char s[] = "Hello";
+    uint16_t prefix = 0xd17f;
+    uint8_t suffix = 0x8c;
+    uint8_t digest[32];
+    uint8_t end[35];
     
+    cout << s;
+
+    // bbp_sha256(digest, )
     Block b1 = Block(0,str1);
     return 0;
 } 
