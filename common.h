451dec3a58d9ead05bced429ffbca0f7375bba49
Timothy Ko <tko@spikes.com>
2017-10-06 00:23:34 -0500

    added common header

---
+++ common.h
@@ -0,0 +1,15 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <iostream>
+#include <string>
+
+void print_hex(const char *label, const uint8_t *v, size_t len) {
+    size_t i;
+
+    printf("%s: ", label);
+    for (i = 0; i < len; ++i) {
+        printf("%02x", v[i]);
+    }
+    printf("\n");
+}
